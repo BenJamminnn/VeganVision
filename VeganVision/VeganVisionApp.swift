84129ba5b5d3428a0113ddbf3d5b3bca1d0f66e8
Ben G <bengabay89@gmail.com>
2022-11-19 14:45:03 -0500

    Initial Commit

---
+++ VeganVision/VeganVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeganVisionApp.swift
+//  VeganVision
+//
+//  Created by Ben Gabay on 11/19/22.
+//
+
+import SwiftUI
+
+@main
+struct VeganVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
